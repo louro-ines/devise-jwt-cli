8f69d0908840ffa0920383d3bcf6245d60d9ef9e
Ines Louro <inespazlouro@gmail.com>
2023-12-27 10:35:54 +0000

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import NormalRoute from './components/NormalRoute';
+import ProtectedRoute from './components/ProtectedRoute';
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' component={NormalRoute} />
+          <Route exact path='/protected_route' component={ProtectedRoute} />
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/login' component={Login} />
+        </Routes>
+      </Router>
     </div>
   );
 }
